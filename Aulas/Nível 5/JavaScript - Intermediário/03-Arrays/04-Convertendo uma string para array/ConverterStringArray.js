2b49afc3a4a5b3e1860fd31e464609913a0a7f8d
AlexandreCalore~ <caloredev@gmail.com~>
2025-09-16 13:23:49 -0300

    Estudos do dia - Convertendo string em array

---
+++ Aulas/Nível 5/JavaScript - Intermediário/03-Arrays/04-Convertendo uma string para array/ConverterStringArray.js
@@ -0,0 +1,10 @@
+//Convertendo string para array.
+
+let fullName = "Alexandre Ricardo Calore"
+console.log(fullName)
+
+//criamos um array com os nomes separando pelo espaço
+console.log(fullName.split(" "))
+
+//Cria um array com as letras
+console.log(Array.from(fullName)) //Aqui estamos dizendo que, vamos criar um array a partir do texto, e nos retorna índices de cada letra do texto.
