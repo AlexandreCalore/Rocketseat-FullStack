8f40b3f66f943063460d633da6e3a159299d8367
Alexandre <caloredev@gmail.com>
2025-09-17 06:44:59 -0300

    Estudos do dia -  Shift/Unshift/Pop/Push

---
+++ Aulas/Nível 5/JavaScript - Intermediário/03-Arrays/05-Adicionando e removendo um item do array/AddRemoveItem.js
@@ -0,0 +1,16 @@
+let users = []
+console.log(users)
+
+users.push("Alexandre")
+users.push("Ricardo")
+users.push("Calore") // Estamos adicionando no final da lista
+console.log(users)
+
+users.unshift("Alexandre") // Adiciona o item no inícios do array
+console.log(users)
+
+users.shift() // Este método remove o item do início do array
+console.log(users)
+
+users.pop() // Ja este, remove do final do array
+console.log(users)
