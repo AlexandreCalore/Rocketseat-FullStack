3a76d731282eb2120dc0ccfa3d82092a100c127f
AlexandreCalore~ <caloredev@gmail.com~>
2025-09-22 07:29:30 -0300

    Estudos do dia - Verificando conteúdo no Array

---
+++ Aulas/Nível 5/JavaScript - Intermediário/03-Arrays/08-Verificando se existe conteúdo no array/VerificandoConteudo.js
@@ -0,0 +1,4 @@
+let fruits = ["Apple", "Orange", "Banana"]
+
+//verifica se um item existe no Array.
+console.log(fruits.includes("Apple"))//Determina se o elemento está incluso no Array. Retornando True/False.
