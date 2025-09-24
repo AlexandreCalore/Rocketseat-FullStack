b813e6a65ba540e6e0d38361f2001f2d25b177d0
AlexandreCalore~ <caloredev@gmail.com~>
2025-09-24 13:21:42 -0300

    Estudos do dia - While

---
+++ Aulas/Nível 5/JavaScript - Intermediário/04-Repetição e iteração/02-Loop While/While.js
@@ -0,0 +1,10 @@
+let execute = true
+
+while (execute) {
+  let response = window.prompt("Deseja continuar: 1(SIM) ou 2(NÃO)") 
+
+  if (response === "2"){
+    execute = false
+    console.log("Operação encerrada")
+  }
+}
