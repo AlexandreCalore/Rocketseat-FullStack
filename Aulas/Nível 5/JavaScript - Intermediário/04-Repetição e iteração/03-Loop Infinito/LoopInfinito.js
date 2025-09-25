fbb3288df044252a0cd4a9e99c9c399d341320fd
AlexandreCalore~ <caloredev@gmail.com~>
2025-09-25 06:48:39 -0300

    Estudos do dia - Loop Infinito

---
+++ Aulas/Nível 5/JavaScript - Intermediário/04-Repetição e iteração/03-Loop Infinito/LoopInfinito.js
@@ -0,0 +1,6 @@
+let value = true
+
+while (value) {
+  console.log('Executando o while')
+}
+//Quando uma condição faz com que a repetição seja executada sem fim. Vai impactar principalmente a performance em geral.
