30b99d0151b99205e4276405713785ddab561167
AlexandreCalore~ <caloredev@gmail.com~>
2025-10-04 14:32:40 -0300

    Estudos do dia - For of

---
+++ Aulas/Nível 5/JavaScript - Intermediário/04-Repetição e iteração/07-For Of/ForOf.js
@@ -0,0 +1,7 @@
+//for of itera sobre valores de um objeto iterável
+
+let students = ["Alexandre", "Alessandra", "John"]
+
+for (let student of students){ //of mostra o conteúdo, in mostra os índices
+  console.log(student)
+}
