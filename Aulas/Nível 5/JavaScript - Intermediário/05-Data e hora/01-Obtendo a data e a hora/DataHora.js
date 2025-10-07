a8d11e413a4860bc2002bc992f5caf68ed83dd71
AlexandreCalore~ <caloredev@gmail.com~>
2025-10-07 07:04:36 -0300

    Estudos do dia - Data e hora

---
+++ Aulas/Nível 5/JavaScript - Intermediário/05-Data e hora/01-Obtendo a data e a hora/DataHora.js
@@ -0,0 +1,7 @@
+//Exibe a data e hora atual, de acordo com o número passado entre os parênteses
+console.log(new Date())
+
+//Utilizando métodos do Date
+
+console.log(new Date().getTime) //Retorna milissegundos com base na data de referência
+
