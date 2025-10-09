594175e88d1925907fed292e0d61a028561dc047
AlexandreCalore~ <caloredev@gmail.com~>
2025-10-09 12:57:52 -0300

    Estudos do dia - Métodos para manipular data

---
+++ Aulas/Nível 5/JavaScript - Intermediário/05-Data e hora/03-Métodos para trabalhar com data e hora/metodosDataHora.js
@@ -0,0 +1,19 @@
+let date = new Date("2025-10-09T12:55")
+
+//Dia da semana contando de 0-6 (Dom = 0 / Sab = 6)
+console.log(date.getDay)
+
+//Dia do mês de 0-30 (e nao 31)
+console.log(date.getDate())
+
+//Mês de 0-11
+console.log(date.getMonth())
+
+//Horas
+console.log(date.getHours())
+
+//Minutos
+console.log(date.getMinutes())
+
+//Segundos
+console.log(date.getSeconds())
