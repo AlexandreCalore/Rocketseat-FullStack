8541c4f4650bc4d2bdaebb670913890a74a1d637
AlexandreCalore~ <caloredev@gmail.com~>
2025-10-13 07:18:16 -0300

    Estudos do dia - Manipulando data e hora

---
+++ Aulas/Nível 5/JavaScript - Intermediário/05-Data e hora/04-Modificando data e hora/MoficandoDataHora.js
@@ -0,0 +1,19 @@
+let date = new Date("July 3, 2025, 13:03:00")
+
+//Modificando o ano.
+date.setFullYear(2030)
+console.log(date)
+
+date.setMonth(7)
+console.log(date)
+
+date.setDate(29)
+console.log(date)
+
+date.setHours(13,23,32) //Horas - Minutos - segundos
+console.log(date)
+
+date.setHours(20)   //Horas
+date.setMinutes(30) //Minutos
+date.setSeconds(30) //Segundos}
+
