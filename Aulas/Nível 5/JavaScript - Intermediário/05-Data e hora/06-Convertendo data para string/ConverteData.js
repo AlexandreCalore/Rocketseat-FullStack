cb9ce675453f202da3035d02d6082d32b7527fec
AlexandreCalore~ <caloredev@gmail.com~>
2025-10-14 06:58:24 -0300

    Estudos do dia - Convertendo somente data ou hora para String

---
+++ Aulas/Nível 5/JavaScript - Intermediário/05-Data e hora/06-Convertendo data para string/ConverteData.js
@@ -0,0 +1,11 @@
+let date = new Date("July 3, 2025, 13:03:00")
+
+//Converte para string
+
+console.log(date.toString())
+
+//Retorna somente data
+console.log(date.toDateString())
+
+//Retorna somente hora
+console.log(date.toTimeString())
