a1f469810a42fe6b30501c24d325772b29190703
AlexandreCalore~ <caloredev@gmail.com~>
2026-01-08 06:50:00 -0300

    Estudos do dia - Implementando funcionalidade de cancelamento de agendamentos | Adicionando lógica para requisição de cancelamento e recarregamento de horários

--- Aulas/Nível 5/03-JavaScritp - Antes do Framework/10-Projeto pratico - Hair Day/01 - Instalações - JSON-SERVER - Webpack server/src/modules/schedules/cancel.js
+++ Aulas/Nível 5/03-JavaScritp - Antes do Framework/10-Projeto pratico - Hair Day/01 - Instalações - JSON-SERVER - Webpack server/src/modules/schedules/cancel.js
@@ -1,19 +1,30 @@
+import { schedulesDay } from "./load.js";
+import { scheduleCancel } from "../../services/schedule-cancel.js";
 const periods = document.querySelector("periods");
 
 //Gera o evento de click para cada lista (manhã tarde e noite)
 periods.forEach((period) => {
   //Captura o evento de clique na lista
-  period.addEventListener("click", (event) => {
+  period.addEventListener("click", async (event) => {
     if(events.target.classList.contains("cancel-icon")) {
       //Obtém a li pai do elemento clicado.
       const item = event.target.closest("li");
+
+      //Pegamos o ID do agendamento para remover
       const { id } = item.dataset;
 
+      //Confirma que o ID foi cancelado
       if(id){
-        const isConfirm = confirm("Deseja realmente cancelar este agendamento?"
+        //Confirma se o usuário quer realmente cancelar
+        const isConfirm = confirm(
+          "Deseja realmente cancelar este agendamento?"
         )
         if(isConfirm){
-          console.log("Cancelando de agendamento confirmado");
+          // Faz a requisição na API para cancelar
+          await scheduleCancel({ id })
+          
+          //Recarrega os agendamentos
+          schedulesDay()
         }
       }
     }
